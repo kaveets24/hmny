36dc179971220459cff44cc02e312ad2391660c9
Steven Kveton <kaveets24@gmail.com>
2018-11-19 18:53:56 +0000

    Delete dev.js

--- config/dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    googleClientID: '702379093055-gg66nua83rjkiqnjkm3m1af7r8emenum.apps.googleusercontent.com',
-    googleClientSecret: 'A-a_evRX8n7z9BWIZ9SChHxg',
-    mongoURI: 'mongodb://kaveets24:Tennischamp24!@ds219191.mlab.com:19191/hmny-dev',
-    cookieKey: '123qweqweasdfasdf'
-}
