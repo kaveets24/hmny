bd8e7cc213db3baa767ca3491b8ae006de579a85
Steven Kveton <stevekaveets24@yahoo.com>
2018-11-25 11:01:15 -0600

    grid and grid item classnames updated

--- client/src/components/Playlists.js
+++ client/src/components/Playlists.js
@@ -4,11 +4,11 @@ const Playlists = () => {
     const playlistNames = ["Playlist 1","Playlist 2","Playlist 3", "Playlist 4", "Playlist 5" ];
     
     const allPlaylists = playlistNames.map((playlist) => {
-        return <div key={playlist} className="main__li">{playlist}</div>
+        return <div key={playlist} className="main__item">{playlist}</div>
     })
 
     return (
-        <div className="main__ul">{allPlaylists}</div>
+        <div className="main__grid">{allPlaylists}</div>
     )
 
 }
