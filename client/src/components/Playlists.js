b6f90fa033ab353c545b2cc9b3aa09a99ad66caf
Steven Kveton <stevekaveets24@yahoo.com>
2018-11-25 21:21:40 -0600

    renamed dummy playlist data

--- client/src/components/Playlists.js
+++ client/src/components/Playlists.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Playlists = () => {
-    const playlistNames = ["Playlist 1","Playlist 2","Playlist 3", "Playlist 4", "Playlist 5", "Playlist 1","Playlist 2","Playlist 3", "Playlist 4", "Playlist 5"];
+    const playlistNames = ["Playlist 1","Playlist 2","Playlist 3", "Playlist 4", "Playlist 5", "Playlist 6","Playlist 7","Playlist 8", "Playlist 9", "Playlist 10"];
     
     const allPlaylists = playlistNames.map((playlist) => {
         return <div key={playlist} className="main__item">{playlist}</div>
