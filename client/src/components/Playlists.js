6c7115da59d27ee3e3ddb97af43d3d0af576b86a
kaveets24 <kaveets24@gmail.com>
2019-04-21 12:40:04 -0500

    updated Playlists.js

--- client/src/components/Playlists.js
+++ client/src/components/Playlists.js
@@ -2,9 +2,13 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-const Playlists = () => {
+const Playlists = (props) => {
     const playlistNames = ["Playlist 1","Playlist 2","Playlist 3", "Playlist 4", "Playlist 5", "Playlist 6","Playlist 7","Playlist 8", "Playlist 9", "Playlist 10"];
     
+    // I will need to figure out how to wait until props.auth.playlists !== null, then grab that playlists object
+    // const playlists = props.auth.playlists;
+    
+    console.log(props.auth)
     const allPlaylists = playlistNames.map((playlist) => {
         return (
             <Link to={`/${playlist}`.replace(' ','_')}>
--- routes/spotifyWrapperRoutes.js
+++ routes/spotifyWrapperRoutes.js
@@ -1,52 +1,45 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const requireLogin = require('../middleware/requireLogin');
-const spotifyApi = require('../services/spotifyWebApi');
-const mongoose = require('mongoose');
-const Track = mongoose.model('tracks');
+const requireLogin = require("../middleware/requireLogin");
+const spotifyApi = require("../services/spotifyWebApi");
+const mongoose = require("mongoose");
+const Track = mongoose.model("tracks");
 // const Playlist = require('../models/Playlist');
 
-router.get('/api/findtrack', requireLogin, async (req, res) => {
-    const user = req.user;
+router.get("/api/findtrack", requireLogin, async (req, res) => {
+  const user = req.user;
 
-    spotifyApi.setAccessToken(user.spotifyAccessToken);
-    spotifyApi.setRefreshToken(user.spotifyRefreshToken);
+  spotifyApi.setAccessToken(user.spotifyAccessToken);
+  spotifyApi.setRefreshToken(user.spotifyRefreshToken);
 
-    const searchResults = await spotifyApi.searchTracks('artist: Anderson Paak', {limit: 5});
+  const searchResults = await spotifyApi.searchTracks("artist: Anderson Paak", {
+    limit: 5
+  });
 
-    const tracks = searchResults.body.tracks.items;
+  const tracks = searchResults.body.tracks.items;
 
-    const matchedTrack = tracks[0];
-    const newTrack = await new Track({
-        trackName: matchedTrack.name,
-        artistName: matchedTrack.artists[0].name,
-        albumName: 'Single',
-        spotifyUri: matchedTrack.uri,
-    }).save();
+  const matchedTrack = tracks[0];
+  const newTrack = await new Track({
+    trackName: matchedTrack.name,
+    artistName: matchedTrack.artists[0].name,
+    albumName: "Single",
+    spotifyUri: matchedTrack.uri
+  }).save();
 
-
-
-    res.send(matchedTrack);
-
-
+  res.send(matchedTrack);
 });
 
+// Should include a regex or pattern matcher for the current playlist, and will begin playback of the first song.
+router.get("/api/play", async (req, res) => {
+  const user = req.user;
+  spotifyApi.setAccessToken(user.spotifyAccessToken);
+  spotifyApi.setRefreshToken(user.spotifyRefreshToken);
 
-// Should include a regex or pattern matcher for the current playlist, and will begin playback of the first song. 
-router.get('/api/play', async (req, res) => {
-    const user = req.user;
-    spotifyApi.setAccessToken(user.spotifyAccessToken);
-    spotifyApi.setRefreshToken(user.spotifyRefreshToken);
+  const spotifyUri = "spotify:album:5oSVYKZLKGCmwYqmJ7AZnO";
 
-    const spotifyUri = "spotify:album:5oSVYKZLKGCmwYqmJ7AZnO";
+  const play = await spotifyApi.play({ context_uri: spotifyUri });
 
-   const play =  await spotifyApi.play({"context_uri": spotifyUri});
-
-    console.log(play);
-    
-
-  
-
+  console.log(play);
 });
 
-module.exports = router;    
+module.exports = router;
