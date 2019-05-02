04c23a42ab3d24d2af68d0def0bc3aa3c47c99bf
Steven Kveton <steven.kveton@designory.com>
2019-05-02 16:12:53 -0500

    added search__results overlay to Search component

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,9 +1,16 @@
 import React, { Component } from "react";
 
 class Search extends Component {
-  state = {
-    inputText: ""
-  };
+  constructor(props) {
+    super(props);
+    this.overlay = React.createRef();
+    this.state = {
+      inputText: "",
+      overlay: false,
+ 
+    };
+  }
+
 
   handleChange = e => {
     this.setState({
@@ -15,18 +22,25 @@ class Search extends Component {
     // make a request to fetch search query (results limited to 20)
   };
 
+  handleClick = () => {
+    this.setState({overlay: true})
+  }
+
+
   render() {
+    let overlayClassName = this.state.overlay ? `search__results`: `search__results search__results--hidden`;
     return (
       <div className="search">
         <form onSubmit={this.handleSubmit}>
           <input
             onChange={this.handleChange}
+            onClick={this.handleClick}
             value={this.state.inputText}
             type="text"
             placeholder="Search for more tracks..."
           />
           <fieldset>
-            <div className="radio-fields">
+            <div className="search__radio-fields">
               <input type="radio" value="spotify" id="spotify" name="music-service" />
               <label for="spotify">Spotify</label>
               <input type="radio" value="youtube" id="youtube" name="music-service" />
@@ -36,6 +50,9 @@ class Search extends Component {
             </div>
           </fieldset>
         </form>
+        <div ref={this.overlay} className={overlayClassName}>
+
+        </div>
       </div>
     );
   }
