8df279773820a760df2961ad96a3e1ca3ebc61e7
Steven Kveton <steven.kveton@designory.com>
2019-07-11 14:48:14 -0500

    removed additional google auth code

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 // Routes
-app.use('/', googleAuth);
+// app.use('/', googleAuth);
 app.use('/', spotifyAuth);
 app.use('/', searchAndPlayer); 
 app.use('/', playlists);
